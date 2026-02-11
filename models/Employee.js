3419b6678ace438f68aa191e91610f76822a941f
RishiIntime <rishi.intime@genus.in>
2026-02-11 11:24:55 +0530

    committing changes related to emplyeelogin validation

--- models/Employee.js
+++ models/Employee.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-  employeeId: { type: String, required: true, unique: true },
-  employeeName: { type: String, required: true },
+  employeeId: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return /^\d+$/.test(v);
+      },
+      message: 'Employee ID must contain only integers'
+    }
+  },
+  employeeName: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: 'Employee name must contain only English letters'
+    }
+  },
   savedAddress: {
     building: String,
     floor: String,
